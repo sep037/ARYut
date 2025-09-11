6634670cfcd3b461667e2c6a4ed13d3103ec0dcf
Yeonsoo So <janu10.sys@gmail.com>
2025-09-11 20:34:31 +0900

    refactor: 파일 위치 수정

--- Yut/Yut/Features/Lobby/Models/PlayerModel.swift
+++ Yut/Yut/Features/Lobby/Models/PlayerModel.swift
@@ -16,7 +16,6 @@ class PlayerModel: Identifiable, ObservableObject, Codable, Equatable, Hashable 
     @Published var isHost: Bool
 
     var pieces: [PieceModel] = []
-//    var pieceEntities: [Entity] = []
     
     static let fileNames = ["Piece1_yellow", "Piece2_jade", "Piece3_blue", "Piece4_red"]
 
@@ -36,7 +35,6 @@ class PlayerModel: Identifiable, ObservableObject, Codable, Equatable, Hashable 
         default:
             fatalError("Invalid player sequence: \(sequence)")
         }
-        /*"Player/P\(sequence)/token"*/
     }
 
     enum CodingKeys: String, CodingKey {
@@ -49,28 +47,21 @@ class PlayerModel: Identifiable, ObservableObject, Codable, Equatable, Hashable 
         self.sequence = sequence
         self.peerID = peerID
         self.isHost = isHost
-//        self.pieceEntities = entities
-//
-//                let dummyEntities = Array(repeating: Entity(), count: 4)
-//                self.pieceEntities = dummyEntities
 
         for _ in 0..<2 {
             guard let piece = PieceModel(owner: self) else { return }
-//            let piece = PieceModel(owner: self, entity: dummyEntities[i])
             pieces.append(piece)
         }
     }
 
     required convenience init(from decoder: Decoder) throws {
             let container = try decoder.container(keyedBy: CodingKeys.self)
-            let id = try container.decode(UUID.self, forKey: .id)
             let name = try container.decode(String.self, forKey: .name)
             let sequence = try container.decode(Int.self, forKey: .sequence)
             let isHost = try container.decode(Bool.self, forKey: .isHost)
             let peerDisplayName = try container.decode(String.self, forKey: .peerDisplayName)
 
             // Entity 임의로 주입
-            let dummyEntities = Array(repeating: Entity(), count: 4)
             self.init(name: name, sequence: sequence, peerID: MCPeerID(displayName: peerDisplayName), isHost: isHost)
         }
 
@@ -90,19 +81,8 @@ class PlayerModel: Identifiable, ObservableObject, Codable, Equatable, Hashable 
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
     }
-
+    
     static func load(name: String, sequence: Int, peerID: MCPeerID, isHost: Bool = false) async -> PlayerModel {
-//        var loadedEntities: [Entity] = []
-
-//        for file in fileNames {
-//            do {
-//                let entity = try await Entity(named: file)
-//                loadedEntities.append(entity)
-//            } catch {
-//                print("'\(file)' 로딩 실패:", error)
-//            }
-//        }
-
         return PlayerModel(name: name, sequence: sequence, peerID: peerID, isHost: isHost)
     }
 }
